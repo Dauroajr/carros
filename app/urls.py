7408706bf36c105f1d622aa702b05eb453617e65
Dauroajr <dauroajr.coding@gmail.com>
2023-08-11 11:50:52 -0300

    updated .html and urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 
